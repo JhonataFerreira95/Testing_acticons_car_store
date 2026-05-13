7895f55d9b82d70d975ca54ecf6505be1cf6e9b5
erick souza <erickagente7@gmail.com>
2026-05-13 13:23:15 -0300

    correção de identação

--- TSI4M-LojaCarro-dev/src/main/java/br/org/edu/ifrn/LojaCarro/controllers/MeuController.java
+++ TSI4M-LojaCarro-dev/src/main/java/br/org/edu/ifrn/LojaCarro/controllers/MeuController.java
@@ -1,24 +1,29 @@
 package br.org.edu.ifrn.LojaCarro.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MeuController {
+    
     int count = 0;
-    @RequestMapping("teste")
+
+    @RequestMapping("/teste") // Adicionada a barra para melhor prática
     public String teste() throws Exception {
         count++;
         if(count < 10) {
             return "bom dia";
-        }else{
+        } else {
             throw new Exception("Excedeu o limite");
         }
     }
 
-}
-@PostMapping("/login")
-public String login(@RequestBody String credentials) {
-    // Simulação simplificada de login
-    return "Login realizado com sucesso";
+    
+    @PostMapping("/login")
+    public String login(@RequestBody String credentials) {
+        // Simulação simplificada de login para validar o item 1 da sua atividade
+        return "Login realizado com sucesso";
+    }
 }
