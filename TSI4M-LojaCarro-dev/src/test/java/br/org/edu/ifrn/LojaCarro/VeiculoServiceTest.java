1abd73e0d441341e3b778b2a9b929cf46324782c
erick souza <erickagente7@gmail.com>
2026-05-13 13:10:46 -0300

    adicionado o veicleServiceTest

---
+++ TSI4M-LojaCarro-dev/src/test/java/br/org/edu/ifrn/LojaCarro/VeiculoServiceTest.java
@@ -0,0 +1,41 @@
+package br.org.edu.ifrn.LojaCarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class VeiculoFeatureTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testeCadastroVeiculo() throws Exception {
+        // Simula um POST para cadastrar
+        mockMvc.perform(post("/veiculos")
+                .contentType("application/json")
+                .content("{\"marca\":\"Fiat\", \"modelo\":\"Uno\", \"ano\":2010}"))
+                .andExpect(status().isCreated()); // Ou isOk() dependendo da sua API
+    }
+
+    @Test
+    void testeBuscaVeiculos() throws Exception {
+        mockMvc.perform(get("/veiculos"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testeLogin() throws Exception {
+        // Simula um Login
+        mockMvc.perform(post("/login")
+                .contentType("application/json")
+                .content("{\"username\":\"admin\", \"password\":\"123\"}"))
+                .andExpect(status().isOk());
+    }
+}
